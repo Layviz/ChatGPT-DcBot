9903ef27f1dbde9d51969ea227ea67f6d64fc48e
Layviz <simon01steinbock@gmail.com>
2023-12-02 23:59:31 +0100

    init

---
+++ ChatGPT-DcBot.py
@@ -0,0 +1,40 @@
+import openai
+from openai import AsyncOpenAI
+import discord
+from discord.ext import commands
+
+client = AsyncOpenAI(
+    api_key=""
+)
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Bot ist bereit! {bot.user.name}')
+
+@bot.command(name="Message")
+async def message(ctx, text):
+    try:
+        async with ctx.typing():
+            MODEL = "gpt-3.5-turbo"
+            response = await client.chat.completions.create(
+                model=MODEL,
+                messages=[
+                    {"role": "system", "content": "Fasse dich kurz, duze mich, sei freundlich und hilfsbereit"},
+                    {"role": "user", "content": text},
+                ],
+                temperature=0,
+            )
+            print(response)
+
+            Antwort=response.choices[0].message.content
+            print(Antwort)
+            await ctx.send(Antwort)
+    except Exception as e:
+        print(e)
+
+bot.run('')
