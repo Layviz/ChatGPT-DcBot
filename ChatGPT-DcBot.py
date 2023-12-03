37874bac3ae63e14fb39e0ceccc06b09f88fb989
Jörn Andreesen <jr.andreesen@gmail.com>
2023-12-03 21:50:21 +0100

    token limit and timed clear

--- ChatGPT-DcBot.py
+++ ChatGPT-DcBot.py
@@ -1,9 +1,9 @@
 from openai import AsyncOpenAI
 import discord
-from discord.ext import commands
 import discord.app_commands
 import json
 import logging
+import threading
 
 logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s",level=logging.DEBUG)
 
@@ -23,13 +23,17 @@ client = AsyncOpenAI(
 )
 MODEL = "gpt-3.5-turbo"
 total_token = 0
+TOKEN_LIMIT=22222
 SYSTEM_MESSAGE=f"Du bist ChatGPT-DcBot. Du bist hier um mit den Nutzern interessante Gespräche zu führen. Die Nachrichten könnten von verschiedenen Nutzern kommen, daher beginnt jede Nachricht mit dem Nutzernamen. Wenn eine deiner Antworten an einen Nutzer im speziellen gerichtet ist, verwende ein @-Zeichen gefolgt vom entsprechenden Nutzernamen in der Antwort. Fasse dich kurz und duze die Nutzer."
 message_memory=[
     {"role": "system", "content": SYSTEM_MESSAGE}
 ]
 
+timer = None
+
 async def get_chatgpt_response(prompt):
     global total_token
+    global message_memory
     message_memory.append({"role": "user", "content": prompt})
     response = await client.chat.completions.create(
         model=MODEL,
@@ -41,6 +45,10 @@ async def get_chatgpt_response(prompt):
     message_memory.append({"role":"assistant", "content":antwort})
     total_token = response.usage.total_tokens
     logging.debug(f"Total number of tokens is now {total_token}")
+    if total_token > TOKEN_LIMIT:
+        logging.warning("The current conversation has reached the token limit!")
+        message_memory=message_memory[len(message_memory)//2:]
+        message_memory.insert(0,{"role": "system", "content": SYSTEM_MESSAGE})
     return antwort
 
 @bot.event
@@ -48,13 +56,18 @@ async def on_ready():
     await tree.sync(guild=discord.Object(id=1150429390015037521))
     logging.info(f'{bot.user.name} ist bereit!')
 
+def timed_clear():
+    global message_memory 
+    message_memory = [{"role": "system", "content": SYSTEM_MESSAGE}] 
+    info_str=f"Die bisherige Konversation wurde nach Timeout gelöscht."
+    logging.info(info_str)
 
-#@bot.hybrid_command(name="clear")
+
 @tree.command(name="clear",guild=discord.Object(id=1150429390015037521))
 async def clear(interaction: discord.Interaction):
     global message_memory 
     message_memory = [{"role": "system", "content": SYSTEM_MESSAGE}] 
-    info_str=f"Die bisherige Konversation wurde gelöscht"
+    info_str=f"Die bisherige Konversation wurde gelöscht."
     logging.info(info_str)
     await interaction.response.send_message(info_str)
 
@@ -68,6 +81,7 @@ async def info(interaction: discord.Interaction):
 
 @bot.event
 async def on_message(message):
+    global timer
     logging.debug("on_message event registered")
     logging.debug(f"mentions {message.mentions}")
     if bot.user in message.mentions:
@@ -82,18 +96,12 @@ async def on_message(message):
         async with message.channel.typing():
             content = await get_chatgpt_response(f"{message.author.display_name}: {message.clean_content}")
             await message.reply(content)
+        if timer and timer.isAlive():
+            timer.cancel()
+        else:
+            timer = threading.Timer(60*60*8,timed_clear)
+            timer.start()
         return True
-    # this isn't needed anymore because its covered by the previous check for mentions
-    # elif message.type == discord.MessageType.reply:
-    #     if message.reference and message.reference.message_id:
-    #         orignal_message = await message.channel.fetch_message(message.reference.message_id)
-    #         if orignal_message.author == bot.user:
-    #             logging.debug(f"Got this message: {message.clean_content}")
-    #             async with message.channel.typing():
-    #                 content = "Beispiel Antwort"
-    #                 # content = get_chatgpt_response(message.clean_content)
-    #                 await message.reply(content)
-    #             return True
     return False
 
 bot.run(secrets["discord-bot.token"])
