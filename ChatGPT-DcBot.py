e9c8cc43802114e49abffdb3b78993547fc828c4
Jörn Andreesen <jr.andreesen@gmail.com>
2023-12-03 20:51:36 +0100

    slash commands and updated system message

--- ChatGPT-DcBot.py
+++ ChatGPT-DcBot.py
@@ -1,6 +1,7 @@
 from openai import AsyncOpenAI
 import discord
 from discord.ext import commands
+import discord.app_commands
 import json
 import logging
 
@@ -10,22 +11,22 @@ logging.info("Loading keys")
 with open("keys.json","r") as fp:
     secrets = json.load(fp)
 
+logging.info("Setting up discord bot")
+intents = discord.Intents.default()
+intents.message_content = True
+bot = discord.Client(intents=intents)
+tree = discord.app_commands.CommandTree(bot)
+
 logging.info("Setting up openai")
 client = AsyncOpenAI(
     api_key=secrets["openai.api-key"]
 )
 MODEL = "gpt-3.5-turbo"
-SYSTEM_MESSAGE="Fasse dich kurz, duze mich, sei freundlich und hilfsbereit"
-
+total_token = 0
+SYSTEM_MESSAGE=f"Du bist ChatGPT-DcBot. Du bist hier um mit den Nutzern interessante Gespräche zu führen. Die Nachrichten könnten von verschiedenen Nutzern kommen, daher beginnt jede Nachricht mit dem Nutzernamen. Wenn eine deiner Antworten an einen Nutzer im speziellen gerichtet ist, verwende ein @-Zeichen gefolgt vom entsprechenden Nutzernamen in der Antwort. Fasse dich kurz und duze die Nutzer."
 message_memory=[
     {"role": "system", "content": SYSTEM_MESSAGE}
 ]
-total_token = 0
-
-logging.info("Setting up discord bot")
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
 
 async def get_chatgpt_response(prompt):
     global total_token
@@ -44,25 +45,25 @@ async def get_chatgpt_response(prompt):
 
 @bot.event
 async def on_ready():
+    await tree.sync(guild=discord.Object(id=1150429390015037521))
     logging.info(f'{bot.user.name} ist bereit!')
 
 
-@bot.hybrid_command(name="clear")
-async def clear(ctx):
+#@bot.hybrid_command(name="clear")
+@tree.command(name="clear",guild=discord.Object(id=1150429390015037521))
+async def clear(interaction: discord.Interaction):
     global message_memory 
     message_memory = [{"role": "system", "content": SYSTEM_MESSAGE}] 
     info_str=f"Die bisherige Konversation wurde gelöscht"
     logging.info(info_str)
-    async with ctx.typing():
-        await ctx.send(info_str)
+    await interaction.response.send_message(info_str)
 
-@bot.hybrid_command(name="info")
-async def info(ctx):
+@tree.command(name="info",guild=discord.Object(id=1150429390015037521))
+async def info(interaction: discord.Interaction):
     messages_len = len(message_memory)
-    info_str=f"Diese Konversation besteht zur Zeit aus {messages_len} Nachrichten"
+    info_str=f"Diese Konversation besteht zur Zeit aus {messages_len} Nachrichten. Das entspricht {total_token} Tokens."
     logging.info(info_str)
-    async with ctx.typing():
-        await ctx.send(info_str)
+    await interaction.response.send_message(info_str)
 
 
 @bot.event
@@ -79,7 +80,7 @@ async def on_message(message):
             return False
         logging.debug(f"Got this message: {message.clean_content}")
         async with message.channel.typing():
-            content = await get_chatgpt_response(message.clean_content)
+            content = await get_chatgpt_response(f"{message.author.display_name}: {message.clean_content}")
             await message.reply(content)
         return True
     # this isn't needed anymore because its covered by the previous check for mentions
@@ -96,3 +97,4 @@ async def on_message(message):
     return False
 
 bot.run(secrets["discord-bot.token"])
+
